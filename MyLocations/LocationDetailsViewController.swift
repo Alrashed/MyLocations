268930ef5458812eed1f30cd622b95ad40d1a73a
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-04-28 20:52:30 -0700

    Design LocationDetailsViewController and hook up its outlets

---
+++ MyLocations/LocationDetailsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by Khalid Alrashed on 4/27/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UITableViewController {
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBAction func done() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
