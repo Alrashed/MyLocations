3b175a625ba81cdee54c8c905f6df8efa173eb66
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-05-01 14:21:49 -0700

    Pass placemark to Tag Location view and resize cells

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -18,6 +18,7 @@ class LocationDetailsViewController: UITableViewController {
     @IBOutlet weak var dateLabel: UILabel!
     
     var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    var placemark: CLPlacemark?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +28,36 @@ class LocationDetailsViewController: UITableViewController {
         
         latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
         longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
+        
+        if let placemark = placemark {
+            addressLabel.text = string(from: placemark)
+        } else {
+            addressLabel.text = "No Address Found"
+        }
+    }
+    
+    func string(from placemark: CLPlacemark) -> String {
+        var text = ""
+        
+        if let s = placemark.subThoroughfare {
+            text += s + " "
+        }
+        if let s = placemark.thoroughfare {
+            text += s + ", "
+        }
+        if let s = placemark.locality {
+            text += s + ", "
+        }
+        if let s = placemark.administrativeArea {
+            text += s + " "
+        }
+        if let s = placemark.postalCode {
+            text += s + ", "
+        }
+        if let s = placemark.country {
+            text += s
+        }
+        return text
     }
     
     @IBAction func done() {
@@ -35,6 +66,23 @@ class LocationDetailsViewController: UITableViewController {
     
     @IBAction func cancel() {
         dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return 88
+            
+        } else if indexPath.section == 2 && indexPath.row == 2 {
+            addressLabel.frame.size = CGSize(width: view.bounds.size.width - 115, height: 44)
+            addressLabel.sizeToFit()
+            addressLabel.frame.origin.x = view.bounds.size.width - addressLabel.frame.size.width - 15
+            return addressLabel.frame.size.height + 20
+            
+        } else {
+            return 44
+        }
     }
 }
 
