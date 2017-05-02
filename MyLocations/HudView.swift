3cfdf4bd9d5a72a1abf0b7f605ce9e30c53d1d35
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-05-01 18:53:10 -0700

    override draw() for HudView

--- MyLocations/HudView.swift
+++ MyLocations/HudView.swift
@@ -12,14 +12,25 @@ class HudView: UIView {
     var text = ""
     
     class func hud(inView view: UIView, animated: Bool) -> HudView {
-        let hudView = HudView(frame: CGRect(x: (view.bounds.width/2) - 50, y: (view.bounds.height/2) - 100, width: 100, height: 100))
+        let hudView = HudView(frame: view.bounds)
         hudView.isOpaque = false
-        hudView.layer.cornerRadius = 5.0
         
         view.addSubview(hudView)
         view.isUserInteractionEnabled = false
         
-        hudView.backgroundColor = UIColor(white: 0.3, alpha: 0.5)
         return hudView
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let boxWidth: CGFloat = 96
+        let boxHeight: CGFloat = 96
+        
+        let boxRect = CGRect(x: round((bounds.size.width - boxWidth) / 2),
+                             y: round((bounds.size.height - boxHeight) / 2),
+                             width: boxWidth, height: boxHeight)
+        
+        let roundedRect = UIBezierPath(roundedRect: boxRect, cornerRadius: 10)
+        UIColor(white: 0.3, alpha: 0.8).setFill()
+        roundedRect.fill()
     }
 }
