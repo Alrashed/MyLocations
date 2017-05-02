9a366241707c08a2b2205e2f209e18b7e3577930
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-05-02 14:32:32 -0700

    Add core data DataModel and create Location entity

---
+++ MyLocations/Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Khalid Alrashed on 5/2/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
---
+++ MyLocations/Location+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Location+CoreDataProperties.swift
+//  MyLocations
+//
+//  Created by Khalid Alrashed on 5/2/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import CoreLocation
+
+
+extension Location {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Location> {
+        return NSFetchRequest<Location>(entityName: "Location")
+    }
+
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+    @NSManaged public var date: NSDate
+    @NSManaged public var locationDescription: String
+    @NSManaged public var category: String
+    @NSManaged public var placemark: CLPlacemark?
+
+}
