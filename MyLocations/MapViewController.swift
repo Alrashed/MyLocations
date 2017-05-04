ba83b2a093c9cef824ae2363968be0ab613ed321
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-05-03 17:59:11 -0700

    Show tagged locations as annotations in mapView

--- MyLocations/MapViewController.swift
+++ MyLocations/MapViewController.swift
@@ -14,13 +14,31 @@ class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
     var managedObjectContext: NSManagedObjectContext!
+    var locations = [Location]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateLocations()
+    }
+    
+    func updateLocations() {
+        mapView.removeAnnotations(locations)
+        
+        let entity = Location.entity()
+        
+        let fetchRequest = NSFetchRequest<Location>()
+        fetchRequest.entity = entity
+        
+        locations = try! managedObjectContext.fetch(fetchRequest)
+        mapView.addAnnotations(locations)
+    }
     
     @IBAction func showUser() {
         let region = MKCoordinateRegionMakeWithDistance(mapView.userLocation.coordinate, 1000, 1000)
         mapView.setRegion(region, animated: true)
     }
     
-    @IBAction func getLocations() {
+    @IBAction func showLocations() {
         
     }
 }
